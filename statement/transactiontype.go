e731687a0909bddce53f989fe32715cd2b930176
Jose F. Mingorance Puga <jose@homa.co>
2020-12-25 15:34:06 +0700

    Fixes mapping of transaction types

--- statement/transactiontype.go
+++ statement/transactiontype.go
@@ -7,7 +7,7 @@ type AllTransactionTypes struct {
 	CashDeposit          TransactionType
 	ChequeMoneyTransfer  TransactionType
 	TransferWithdrawal   TransactionType
-	Comission            TransactionType
+	Commission           TransactionType
 	LetterCredit         TransactionType
 	InterestDeposit      TransactionType
 	WitholdingTaxPayable TransactionType
@@ -18,7 +18,7 @@ func GetAllTransactionTypes() AllTransactionTypes {
 		CashDeposit:          "Cash Deposit",
 		ChequeMoneyTransfer:  "Cheque/Money Transfer",
 		TransferWithdrawal:   "Transfer Withdrawal",
-		Comission:            "Comission",
+		Commission:           "Commission",
 		LetterCredit:         "Letter of Credit",
 		InterestDeposit:      "Interest Deposit",
 		WitholdingTaxPayable: "Witholding Tax Payable",
@@ -28,21 +28,32 @@ func GetAllTransactionTypes() AllTransactionTypes {
 func GetAllTransactionTypesMap() map[string]TransactionType {
 	allTransactionTypes := GetAllTransactionTypes()
 	return map[string]TransactionType{
-		string(allTransactionTypes.CashDeposit):         (allTransactionTypes.CashDeposit),
-		string(allTransactionTypes.ChequeMoneyTransfer): allTransactionTypes.ChequeMoneyTransfer,
-		string(allTransactionTypes.TransferWithdrawal):  allTransactionTypes.TransferWithdrawal,
+		sanitizeTransactionTypeString(string(allTransactionTypes.CashDeposit)):          allTransactionTypes.CashDeposit,
+		sanitizeTransactionTypeString(string(allTransactionTypes.ChequeMoneyTransfer)):  allTransactionTypes.ChequeMoneyTransfer,
+		sanitizeTransactionTypeString(string(allTransactionTypes.TransferWithdrawal)):   allTransactionTypes.TransferWithdrawal,
+		sanitizeTransactionTypeString(string(allTransactionTypes.Commission)):           allTransactionTypes.Commission,
+		sanitizeTransactionTypeString(string(allTransactionTypes.LetterCredit)):         allTransactionTypes.LetterCredit,
+		sanitizeTransactionTypeString(string(allTransactionTypes.InterestDeposit)):      allTransactionTypes.InterestDeposit,
+		sanitizeTransactionTypeString(string(allTransactionTypes.WitholdingTaxPayable)): allTransactionTypes.WitholdingTaxPayable,
 	}
 }
 
 var allTransactionTypesMap = GetAllTransactionTypesMap()
 
 func transactionTypeFromString(asString string) *TransactionType {
-	asString = strings.ReplaceAll(asString, " NB", "")
-	//asString = strings.ReplaceAll(asString, " ", "")
-
-	tt, exists := allTransactionTypesMap[asString]
+	sanitizedString := sanitizeTransactionTypeString(asString)
+	tt, exists := allTransactionTypesMap[sanitizedString]
 	if !exists {
 		return nil
 	}
 	return &tt
+}
+
+func sanitizeTransactionTypeString(dangerousString string) string {
+
+	newString := strings.ReplaceAll(dangerousString, " NB", "")
+	newString = strings.ReplaceAll(newString, " ", "")
+	newString = strings.ToLower(newString)
+
+	return newString
 }
