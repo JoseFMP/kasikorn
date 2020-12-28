ab0dab94044c498c8dd4ff3230c06b0d07b8cfde
Jose F. Mingorance Puga <jose@homa.co>
2020-12-28 13:36:26 +0700

    Adds more transactions

--- transaction/transactiontype.go
+++ transaction/transactiontype.go
@@ -4,6 +4,8 @@ import "strings"
 
 type TransactionType string
 type AllTransactionTypes struct {
+	CashWithdrawal       TransactionType
+	ClearingCheque       TransactionType
 	CashDeposit          TransactionType
 	ChequeMoneyTransfer  TransactionType
 	TransferWithdrawal   TransactionType
@@ -15,8 +17,11 @@ type AllTransactionTypes struct {
 
 func GetAllTransactionTypes() AllTransactionTypes {
 	return AllTransactionTypes{
+
 		CashDeposit:          "Cash Deposit",
+		CashWithdrawal:       "Cash Withdrawal",
 		ChequeMoneyTransfer:  "Cheque/Money Transfer",
+		ClearingCheque:       "Clearing Cheque",
 		TransferWithdrawal:   "Transfer Withdrawal",
 		Commission:           "Commission",
 		LetterCredit:         "Letter of Credit",
@@ -29,7 +34,9 @@ func GetAllTransactionTypesMap() map[string]TransactionType {
 	allTransactionTypes := GetAllTransactionTypes()
 	return map[string]TransactionType{
 		sanitizeTransactionTypeString(string(allTransactionTypes.CashDeposit)):          allTransactionTypes.CashDeposit,
+		sanitizeTransactionTypeString(string(allTransactionTypes.CashWithdrawal)):       allTransactionTypes.CashWithdrawal,
 		sanitizeTransactionTypeString(string(allTransactionTypes.ChequeMoneyTransfer)):  allTransactionTypes.ChequeMoneyTransfer,
+		sanitizeTransactionTypeString(string(allTransactionTypes.ClearingCheque)):       allTransactionTypes.ClearingCheque,
 		sanitizeTransactionTypeString(string(allTransactionTypes.TransferWithdrawal)):   allTransactionTypes.TransferWithdrawal,
 		sanitizeTransactionTypeString(string(allTransactionTypes.Commission)):           allTransactionTypes.Commission,
 		sanitizeTransactionTypeString(string(allTransactionTypes.LetterCredit)):         allTransactionTypes.LetterCredit,
